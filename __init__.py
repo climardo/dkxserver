b77d0faf27da69fbfb8d4a4ac7bf728f2755d415
Carlos Limardo <climardo@gmail.com>
2020-10-10 23:39:22 -0400

    Integrate dkexrtact and add basic functionality

--- __init__.py
+++ __init__.py
@@ -2,11 +2,12 @@ import dkextract, json
 from requests import Session
 from flask import Flask, flash, render_template, request, redirect, session, url_for
 from flask_wtf import FlaskForm
+from os import environ
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='UD1KbD__RZujhpV2p6r9MQ',
+        SECRET_KEY=environ.get('SECRET_KEY'),
     )
 
     all_members = set(["arianna29", "climardo", "DarbyDiaz3", "Dash7", "ejmesa", "frank.corn", "glopez28", "Halvworld", "hlimardo", "JekellP", "jlopez0809", "LoLoGREEN", "olivadotij", "pshhidk", "rogdiaz", "XplicitK", "g_mendoza"])
@@ -20,13 +21,26 @@ def create_app(test_config=None):
 
     @app.route('/', methods=['GET'])
     def index():
-        not_submitted_set = dkextract.get_not_submitted_list(s, 93745891, all_members)
-        not_submitted_json = json.dumps(list(not_submitted_set))
-        return render_template('index.html', not_submitted=not_submitted_json)
+        return render_template('index.html')
     
-    @app.route('/generate', methods=['GET'])
+    @app.route('/generate', methods=['GET', 'POST'])
     def generate():
-        None
+        if request.method == 'GET':
+            return render_template('generate.html')
+        elif request.method == 'POST':
+            if request.form.get('generate'):
+                form = request.form
+                weekly = dkextract.generate_results(s, contest_id=form['contest_id'], week=form['week'], year=form['year'], winning_values=winning_values)
+                return weekly
+            elif request.form.get('get_not_submitted'):
+                form = request.form
+                not_submitted = dkextract.get_not_submitted_list(s, contest_id=form['contest_id'], all_members=all_members)
+                return render_template('not_submitted.html', not_submitted=not_submitted)
 
+    @app.route('/not_submitted/<contest_id>', methods=['GET', 'POST'])
+    def not_submitted(contest_id):
+        if request.method == 'GET':
+            not_submitted = dkextract.get_not_submitted_list(s, contest_id=contest_id, all_members=all_members)
+            return render_template('not_submitted.html', not_submitted=not_submitted)
 
     return app
