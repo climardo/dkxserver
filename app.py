961c8f88d49be69f1f66203f2185ce1def4154de
Carlos <me@sudocarlos.com>
2024-09-08 14:08:14 -0400

    fix env loading for gunicorn

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from dkxserver import create_app
+from dotenv import load_dotenv
+
+load_dotenv('.env') #the path to your .env file (or any other file of environment variables you want to load)
 
 app = create_app()
